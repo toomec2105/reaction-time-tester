2f9e851adf4c15fda9a287e915f383944ff74c0e
toomec2105 <tomec2105@interia.pl>
2020-07-25 19:40:44 +0200

    add drawcircle function

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -2,5 +2,22 @@ import { okGame } from './learning-reference/gameMessage';
 
 //console.log(okGame("Football")); //js import  test
 
+let circle1 = document.getElementById("circle1");
+let circle2 = document.getElementById("circle2");
 
+
+circle1.addEventListener("click", function fn(e) {
+   circle1.classList.add("hidden");});
+
+circle2.addEventListener("click", function fn(e) {
+   circle2.classList.add("hidden");});
+
+function drawCircle(time, place){
+  setInterval(function(){ 
+    place.classList.add("visible");},time
+    ); 
+}
+
+drawCircle(3000,circle1);
+drawCircle(3000,circle2);
 
