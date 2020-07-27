2f46cd344fb2d40c04c125037bb969d8a7c4ac10
toomec2105 <tomec2105@interia.pl>
2020-07-27 20:35:01 +0200

    add setCoordinates

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -6,16 +6,17 @@ const canvas = document.getElementById("canvas");
 const canvasTop = document.getElementById("canvasTop");
 const canvasBot = document.querySelector("#canvasBot");
 
-function drawShape(size, color, border_radius, canvasId) {
+function drawShape(size, color, border_radius, canvasId,x,y) {
   canvasId.style.backgroundColor = color;
   canvasId.style.width = size + "px";
   canvasId.style.height = size + "px";
   canvasId.style.borderRadius = border_radius + "%";
   canvasId.classList.add("shape");
+  setCoordinates(x,y,canvasId);
 }
 
-drawShape(200, "red", 50, canvasTop);
-drawShape(300, "lightblue", 0, canvasBot);
+drawShape(200, "red", 50, canvasTop,134,500);
+drawShape(300, "lightblue", 0, canvasBot,700,200);
 
 const shape = document.querySelector(".shape");
 console.log(shape);
@@ -24,3 +25,8 @@ shape.addEventListener("click", function () {
   shape.classList.add("invisible");
 });
 
+function setCoordinates(x_pos, y_pos, d) {
+  d.style.position = "absolute";
+  d.style.left = x_pos+'px';
+  d.style.top = y_pos+'px';
+}
